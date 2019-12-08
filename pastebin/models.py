08086bfbf7118deb4d847d1ae3aa00cba425c006
CSY54 <alex35748954@gmail.com>
2019-12-08 16:59:10 +0800

    Modify table primary key

--- pastebin/models.py
+++ pastebin/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    id = models.CharField(max_length=64, primary_key=True)
-    filename = models.CharField(max_length=32)
+    filename = models.CharField(max_length=64, primary_key=True)
     content = models.CharField(max_length=1024)
     timestamp = models.DateTimeField(auto_now=True)
--- pastebin/views.py
+++ pastebin/views.py
@@ -2,19 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.utils import IntegrityError
 from json import dumps
-from hashlib import sha256
 
 from pastebin.models import Post
 
 # Create your views here.
-def home(request):
+def home(request): 
     if request.method == 'POST':
         filename = request.POST.get('filename')
         content = request.POST.get('content')
 
         res = {}
 
-        if not (0 < len(filename) < 32):
+        if not (0 < len(filename) < 64):
             res['success'] = False
             res['message'] = 'Filename too long'
         elif not (0 < len(content) < 1024):
@@ -23,24 +22,23 @@ def home(request):
 
         try:
             if 'success' not in res:
-                hash = sha256(content.encode('utf-8')).hexdigest()
-                Post.objects.create(id=hash, filename=filename, content=content)
+                Post.objects.create(filename=filename, content=content)
                 res['success'] = True
-                res['id'] = hash
+                res['filename'] = filename 
         except IntegrityError:
             res['success'] = False
-            res['message'] = 'Post duplicated'
+            res['message'] = 'Filename duplicated'
         except:
             res['success'] = False
             res['message'] = 'Unknown error, please contact admin'
 
         return HttpResponse(dumps(res), content_type='application/json')
     else:
-        hash = request.GET.get('id')
+        filename = request.GET.get('filename')
 
-        if hash is not None:
+        if filename is not None:
             try:
-                res = Post.objects.get(id=hash)
+                res = Post.objects.get(filename=filename)
                 return render(request, 'index.html', {
                     'is_post': True,
                     'filename': res.filename,
